3f947cfe0833ace3b7c1f915c1ab93a6d3d1d2ca
Michael Herndon <mherndon@nerdymishka.com>
2020-06-12 12:52:20 -0500

    AB#16 import common xunit code
     - import shared code from xunit that enables building a custom
       xunit test framework/runtime.

---
+++ Mettle/src/Common/ExecutionHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection;
+
+namespace Xunit
+{
+    internal static class ExecutionHelper
+    {
+        /// <summary>
+        /// Gets the substitution token used as assembly name suffix to indicate that the assembly is
+        /// a generalized reference to the platform-specific assembly.
+        /// </summary>
+        public const string SubstitutionToken = ".{Platform}";
+
+        private const string ExecutionAssemblyNamePrefix = "xunit.execution.";
+#if NETSTANDARD
+        private static readonly string[] PlatformSuffixes = new[] { "dotnet", "MonoAndroid", "iOS-Universal" };
+#endif
+
+        private static readonly object Lock = new object();
+
+        private static string platformSuffix = "__unknown__";
+
+        public static string PlatformSuffix
+        {
+            get
+            {
+                lock (Lock)
+                {
+                    if (platformSuffix == "__unknown__")
+                    {
+                        platformSuffix = null;
+
+#if NETSTANDARD
+                        foreach (var suffix in PlatformSuffixes)
+                        {
+                            try
+                            {
+                                Assembly.Load(new AssemblyName { Name = ExecutionAssemblyNamePrefix + suffix });
+                                platformSuffix = suffix;
+                                break;
+                            }
+#pragma warning disable CA1031 // Do not catch general exception types
+                            catch
+#pragma warning restore CA1031 // Do not catch general exception types
+                            {
+                            }
+                        }
+
+#else
+                        foreach (var name in AppDomain.CurrentDomain.GetAssemblies().Select(a => a?.GetName()?.Name))
+                            if (name != null && name.StartsWith(executionAssemblyNamePrefix, StringComparison.Ordinal))
+                            {
+                                platformSuffix = name.Substring(executionAssemblyNamePrefix.Length);
+                                break;
+                            }
+#endif
+                    }
+                }
+
+                if (platformSuffix == null)
+                    throw new InvalidOperationException($"Could not find any xunit.execution.* assembly loaded in the current context");
+
+                return platformSuffix;
+            }
+        }
+    }
+}
---
+++ Mettle/src/GlobalSuppressions.cs
@@ -0,0 +1,28 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:Mettle.Xunit.Sdk.MettleTestAssemblyRunner.Initialize")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1305:Specify IFormatProvider", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1404:Code analysis suppression should have justification", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1202:Elements should be ordered by access", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1101:Prefix local calls with this", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1611:Element parameters should be documented", Justification = "<Pending>")]
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1629:Documentation text should end with a period", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1201:Elements should appear in the correct order", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1202:Elements should be ordered by access", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1721:Property names should not match get methods", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1516:Elements should be separated by blank line", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1721:Property names should not match get methods", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1819:Properties should not return arrays", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1615:Element return value should be documented", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0052:Remove unread private members", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1307:Specify StringComparison", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1616:Element return value documentation should have text", Justification = "<Pending>")]
