fa972b7947a9f7eed224ab0301581b39646fae9b
Michael Herndon <mherndon@nerdymishka.com>
2020-11-22 02:33:15 -0600

    add author changes, add attribute changes

--- dnet/src/Nmx.Core/ValidatedNotNullAttribute.cs
+++ dnet/src/Nmx.Core/ValidatedNotNullAttribute.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace NerdyMishka
 {
+
     [AttributeUsage(AttributeTargets.Parameter, Inherited = false, AllowMultiple = false)]
     public sealed class ValidatedNotNullAttribute : Attribute
     {
